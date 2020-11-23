621bbcc87ec22ac6ab0f712ddad55f756ed8b667
roodey21 <fahrudin.abadi@students.amikom.ac.id>
2020-11-23 09:19:53 +0000

    add model, migration, and controller for brand, category, and product

---
+++ database/migrations/2020_11_20_094204_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('brand_id');
+            $table->string('summary');
+            $table->text('description');
+            $table->string('slug');
+            $table->integer('stock');
+            $table->string('SKU');
+            $table->boolean('status'); // status 0 = draft, 1 = publish
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::view('/test', 'test');
